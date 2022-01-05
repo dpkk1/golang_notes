15370b3d3f3e9455695994ffe3c6874a49a47828
dpkk1 <dpkkmr1600@gmail.com>
2022-01-05 19:20:23 +0530

    compiled first program

---
+++ helloworld.go
@@ -0,0 +1,17 @@
+// First lesson to learn GO Lang, Print "Hello World"
+
+/*
+	Special package which is used with the programs
+  that are executable and this package contains mains function
+*/
+package main
+
+//Importing package for Printing
+import "fmt"
+
+//Function initiation i.e, Entry point of program
+func main() {
+	fmt.Println("Hello World")
+}
+
+
